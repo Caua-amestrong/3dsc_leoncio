81a72d144b695f34f8913579b84a594450d1725a
Caua-amestrong <130366429+Caua-amestrong@users.noreply.github.com>
2024-09-29 14:14:15 -0300

    Add files via upload

---
+++ loopWhile/index.js
@@ -0,0 +1,134 @@
+
+
+//   while(n <=10){      
+//  CTRL + C INTERROMPER 
+//  LOOP INFINITO 
+
+//    console.log(n)     
+//      n++;
+
+// }
+
+
+
+//let n = 1
+//setTimeout(function() {
+//while(true){
+//let n2 = (Math.random() * 60).toFixed(2) 
+//console.log(n2)
+//console.clear()
+//}
+  // },1000);
+
+//WHILE VS DO WHILE 
+// let n= 1
+// do {
+    // console.log("----Menu----");
+    // console.log(("Qual é seu nome?"));
+    // console.log("--------------------");
+  
+// } while(n < 10);
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var meuNumero = Math.floor(Math.random() * 60) + 1; // Gerando um número aleatório entre 1 e 60
+var numeroDeTentativas = 3;
+var tentativas = 0;
+var acertou = false;
+
+ Loop while para as tentativas
+while (tentativas < numeroDeTentativas) {
+    // Pedindo ao usuário para adivinhar o número
+    var palpite = parseInt(prompt("Adivinhe meu número da sorte (entre 1 e 60):"));
+
+    // Verificando se o palpite está correto
+    if (palpite === meuNumero) {
+        acertou = true;
+        break; // Se acertar, sair do loop
+    } else {
+        // Se o palpite estiver errado, informar e reduzir o número de tentativas
+        alert("Tente novamente. Você ainda tem " + (numeroDeTentativas - tentativas - 1) + " tentativa(s) restante(s).");
+        tentativas++;
+    }
+}
+
+// Verificando se o usuário acertou ou não
+if (acertou) {
+    alert("Parabéns! Você acertou meu número da sorte: " + meuNumero);
+} else {
+    alert("Suas tentativas acabaram. Meu número da sorte era: " + meuNumero);
+}
+*/
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+ 
+ 
+
+
+
+
+
+
+
+
