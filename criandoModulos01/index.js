959d338b29419d1b746194a0166f6afdee74af86
Caua-amestrong <130366429+Caua-amestrong@users.noreply.github.com>
2024-09-29 14:02:03 -0300

    Add files via upload

---
+++ criandoModulos01/index.js
@@ -0,0 +1,7 @@
+import { meuNome, caixaAlta, caixaBaixa, imprimeNome } from  './lib/strings.js';
+
+console.log(meuNome);
+console.log(imprimeNome())
+
+console.log(caixaAlta("Renanplay"));
+console.log(caixaBaixa("LuanGameplay"));
