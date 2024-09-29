0013bedc0ba9d5ae6d1e13161f90736f6757c23e
Caua-amestrong <130366429+Caua-amestrong@users.noreply.github.com>
2024-09-29 14:13:07 -0300

    Add files via upload

---
+++ operadorTernarioCelc/index.js
@@ -0,0 +1,15 @@
+// OPERADOR TERNARIO 
+
+// SÃO USADOS PARA SIMPLIFICAR O PROCESSO DE TOMADA DE DECISÃO E, EVENTUALMENTE, 
+//SUBSTITUIR IF/ELSE 
+
+const idade = 18 
+if (idade >= 18) {
+  console.log("Permitida a entrada");
+} else {
+  console.log("Proibido para menores");
+}
+  
+// COM OPERADOR TERNARIO 
+const passe = idade >= 18 ? "Permitida a entrada" : "Proibida a entrada"
+console.log(passe)
